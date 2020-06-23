d1d48db90bc25cac275fb07428a9120f353fcf0e
Frederik Schönfeldt <fschoenfeldt@orion.de>
2020-06-23 12:44:14 +0200

    add comments

--- src/frontend/contextmenu.js
+++ src/frontend/contextmenu.js
@@ -1,6 +1,7 @@
 import ContextMenu from './lib/tui-context-menu';
 
 const registerUserContextMenu = () => {
+    // Kontextmenü-Einstellungen
     const contextMenuContent = [{
             title: 'Stummschalten'
         },
@@ -17,12 +18,15 @@ const registerUserContextMenu = () => {
             ]
         }
     ]
-   
+
+    // Kontextmenü finden und einstellen
     const userContextMenu = new ContextMenu(document.getElementById('sidebar--user__ctx-menu'));
 
+    // Benutzer finden und Kontextmenü bei Klick dort anzeigen lassen
     const users = document.querySelectorAll('div.users > p');
     users.forEach((elem) => userContextMenu.register(`#${elem.id}`, onClick, contextMenuContent));
 
+    // Hier könnte man beim Klick je nach cmd tolle Dinge machen
     function onClick(e, cmd) {
         console.log(cmd);
     }
