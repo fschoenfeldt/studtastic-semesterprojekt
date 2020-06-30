6a462153086e39f99609f20b8d3e6c0007e0e6f2
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2020-06-30 13:22:17 +0200

    fix usernote empty error

--- src/frontend/user-note.js
+++ src/frontend/user-note.js
@@ -8,16 +8,19 @@ const showUserNote = () => {
     const currentUser = $('.current-user');
     const firstUser = $('#user-4');
     const userNoteButton = $('#link-note');
-    userNoteButton.addEventListener("click", (event) => {
-        currentUser.classList.toggle('note-active');
-        if (voiceChatContainer && voiceChatContainer.childElementCount == 1) {
-            // add one person to voice channel
-            $('.voice--users').appendChild(document.createElement("div"));
-        } else {
-            // nothing..?
-        }
-        event.preventDefault();
-    });
+
+    if (userNoteButton) {
+        userNoteButton.addEventListener("click", (event) => {
+            currentUser.classList.toggle('note-active');
+            if (voiceChatContainer && voiceChatContainer.childElementCount == 1) {
+                // add one person to voice channel
+                $('.voice--users').appendChild(document.createElement("div"));
+            } else {
+                // nothing..?
+            }
+            event.preventDefault();
+        });
+    }
 }
 
 export const register = () => {
