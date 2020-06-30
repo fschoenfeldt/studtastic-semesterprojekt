e84ffeb44eaa7d5f33f835fc461f43e04a9edff7
Frederik Schönfeldt <frederikschoenfeldt@gmail.com>
2020-06-30 14:28:35 +0200

    make chat great again!

--- src/frontend/chat.js
+++ src/frontend/chat.js
@@ -3,6 +3,12 @@ import {
     $$
 } from './lib/querySelector';
 
+import {
+    create
+} from './lib/dom-create-element.js';
+
+const currentUsername = $('.current-user').innerHTML;
+
 // https://stackoverflow.com/a/49389811
 const submitTextareaOnSubmit = (event) => {
     if (event.which === 13 && !event.shiftKey) {
@@ -10,28 +16,23 @@ const submitTextareaOnSubmit = (event) => {
             cancelable: true
         }));
         event.preventDefault(); // Prevents the addition of a new line in the text field (not needed in a lot of cases)
-        console.log(event.target.value);
+/*         console.log(event.target.value);
         alert(`Deine Nachricht: ${event.target.value}
         (Füg mich in den Chat ein)`);
-
+ */
 
         const chatMessagesContainer = $('.chat--messages');
-        /* !TODO !IMPORTANT actually insert Chatmessage to chat
         chatMessagesContainer.appendChild(createChatMessage({
-            time: "jetzt",
-            author: "ich",
+            time: `${(new Date()).getHours()}:${(new Date()).getMinutes()}`,
+            author: currentUsername,
             text: event.target.value
-        })); */
+        }));
         console.table(chatMessagesContainer)
         event.target.value = "";
     }
 }
 
-const createChatMessage = ({
-    time,
-    author,
-    text
-}) => {
+const createChatMessage = (message) => {
     // !TODO write function for creating DOM element here
     // https://developer.mozilla.org/de/docs/Web/API/Document/createElement
     /*
@@ -42,6 +43,22 @@ const createChatMessage = ({
     </div>
     
     */
+
+
+    let messageElement = create({
+        selector: 'div',
+        styles: 'message',
+        children: create({
+            selector: 'small',
+            html: `${message.time} von ${message.author}`
+        })
+    });
+    messageElement.appendChild(create({
+        selector: 'p',
+        html : message.text
+    }));
+
+    return messageElement;
 }
 
 export const register = () => {
--- src/studtastic/helper/messages.js
+++ src/studtastic/helper/messages.js
@@ -1,12 +1,37 @@
 export const defaultChatMessages = [{
         time: '11:32',
         author: 'Bob Andrews',
-        text: 'Wie lösche ich denn Aufgabe3??'
+        text: 'Wie lösche ich denn Aufgabe 3??'
     },
     {
         time: '12:02',
         author: 'Peter Shaw',
-        text: 'Mithilfe von Skriptseite 34 kannst du die Aufgabe lösen.'
+        text: 'Mithilfe von Scriptseite 34 kann du die Aufgabe lösen.'
+    },
+    {
+        time: '12:04',
+        author: 'Peter Shaw',
+        text: 'Oder meinst du Löschen, was wie?'
+    },
+    {
+        time: '12:09',
+        author: 'Bob Andrews',
+        text: 'Das hilft mir jetzt aber nicht wirklich weiter.'
+    },
+    {
+        time: '12:14',
+        author: 'Peter Shaw',
+        text: 'Ja du musst mir schon sagen wo das Problem liegt, ich kann die ja nicht für dich lösen.'
+    },
+    {
+        time: '12:18',
+        author: 'Bob Andrews',
+        text: 'Okay, ich komme bei Schritt 5 nicht weiter, es funktioniert nicht mit der Zuweisung.'
+    },
+    {
+        time: '12:45',
+        author: 'Peter Shaw',
+        text: 'Versuch mal das Ergebnis zu Typecasten.'
     },
     {
         time: '14:44',
